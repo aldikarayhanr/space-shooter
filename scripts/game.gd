a80ba041d350dc2ebcd76bf3a185180bc1824780
Aldika Rayhan Ramadhan <aldikarayhanr@gmail.com>
2025-08-26 10:27:54 +0700

    Game Over Scene

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,6 +3,7 @@ extends Node2D
 var lives : int = 3
 @onready var player: CharacterBody2D = $Player
 @onready var hud: Control = $UI/HUD
+@onready var ui: CanvasLayer = $UI
 
 func _ready() -> void:
 	pass
@@ -18,8 +19,16 @@ func on_tabrak():
 	if lives == 0:
 		print("game over")
 		player.died()
+		game_over()
 	else:
 		print(lives)
 
 func update_hud():
 	hud.get_node("Lives").text = "Lives = " + str(lives)
+
+func game_over():
+	var game_over_screen : PackedScene = load("res://scenes/game_over.tscn")
+	var game_over_instantiate = game_over_screen.instantiate()
+	ui.add_child(game_over_instantiate)
+	
+	
